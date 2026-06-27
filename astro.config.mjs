4048ad872aaf0444955392d35076afef993a5607
yuhinniii <yuhinniii@example.com>
2026-06-27 19:29:56 +0800

    feat: update site URL to chinastarterhub.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://china-starter-guide-3sn.pages.dev',
+  site: 'https://chinastarterhub.com',
   output: 'static',
   i18n: {
     defaultLocale: 'en',
